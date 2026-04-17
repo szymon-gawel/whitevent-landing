69c1a9835e414f9fb84137583c2a2811de754c9e
Szymon Gaweł <szymongawel123@gmail.com>
2026-04-17 21:03:18 +0200

    SEO optimalizing

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://whitevent.com',
+      routes: ['/'],
+    }),
+  ],
   build: {
     rollupOptions: {
       output: {
